fed338d02e26fc45fab3cedd2c26f1a5766c08a4
Maalvika Bhat <mbhat@olin.edu>
2020-01-30 19:54:25 -0500

    Lecture 02

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int x = 5;
+    int y = x + 1;
+    printf(y);
     return 0;
 }
+
+% probably takes up less memory and makes it so that we need fewer operations
